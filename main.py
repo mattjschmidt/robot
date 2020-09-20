55745485899268a7cec62274aa7f756edb46f924
Matthew Schmidt <mattschmidt@outlook.com>
2020-09-19 23:02:39 -0400

    Started work on selecting a primary controller with the Start button

--- main.py
+++ main.py
@@ -2,29 +2,45 @@ import evdev
 from evdev import InputDevice, categorize, ecodes
 from controller import Controller, SourceType
 import time
+import threading
+
+class Thread(threading.Thread):
+    def __init__(self, pad):
+        threading.Thread.__init__(self)
+        self.pad = pad
+    def run(self):
+        padSwap(self.pad)
+        
+
+def padSwap(pad):
+    for event in pad.ctrl.read_loop():
+        if event.code == 315 and event.value == 1:
+            print("Connected: " + pad.name)
 
 def main():
     printCount = 0
+    threads = []
     while(True):
-        availableDevs = [InputDevice(path) for path in evdev.list_devices()]
-        if len(availableDevs) == 1:
+        controllers = [Controller(path, SourceType.USB) for path in evdev.list_devices()]
+        if len(controllers) > 0:
             printCount = 0
-            pad = Controller(availableDevs[0].path, SourceType.USB)
-            try:
-                print("Connected: " + pad.name)
-                for event in pad.ctrl.read_loop():
-                    if (event.type == ecodes.EV_KEY or event.type == ecodes.EV_ABS) and event.value == 1:
-                        if event.code == 304:
-                            print("A")
-                        if event.code == 305:
-                            print("B")
-            except:
-                throw
-        elif len(availableDevs) > 1:
-            if printCount == 0:
-                print("Please disconnect any extra controllers to continue...")
-                printCount = 1
-        elif len(availableDevs) == 0:
+            for pad in controllers:
+                if len(threads) > 0:
+                    exists = False
+                    for thrd in threads:
+                        if pad.source == thrd.pad.source:
+                            exists = True
+                    if exists == False:
+                        padThread = Thread(pad)
+                        print("Available device: " + pad.name)
+                        threads.append(padThread)
+                        padThread.start()
+                else: 
+                    padThread = Thread(pad)
+                    print("Added device: " + pad.name)
+                    threads.append(padThread)
+                    padThread.start()
+        else:
             if printCount == 0:
                 print("Please connect a controller to continue...")
                 printCount = 1
