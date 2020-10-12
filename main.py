6b8f3fa43a2e6bdf9615ca2c9dfd5e5ae1c8f810
Matthew Schmidt <mattschmidt@outlook.com>
2020-10-12 13:10:47 -0400

    Updated main method

--- main.py
+++ main.py
@@ -1,53 +1,30 @@
-import evdev
-from evdev import InputDevice, categorize, ecodes
-from controller import Controller, SourceType
-import time
-import threading
-
-threads = []
-
-class Thread(threading.Thread):
-    def __init__(self, pad):
-        threading.Thread.__init__(self)
-        self.pad = pad
-    def run(self):
-        padSwap(self.pad)
-        
-
-def padSwap(pad):
-    try:
-        for event in pad.ctrl.read_loop():
-            if event.code == 315 and event.value == 1:
-                print("Connected: " + pad.name)
-    except:
-        pass
+from gamepad.padService import PadService
+import os
 
 def main():
-    printCount = 0
+
+    padServ = PadService()
+    padsCopy = padServ.pads.copy()
+
     while(True):
-        controllers = [Controller(path, SourceType.USB) for path in evdev.list_devices()]
-        if len(controllers) > 0:
-            printCount = 0
-            for pad in controllers:
-                if len(threads) > 0:
-                    exists = False
-                    for thrd in threads:
-                        if pad.source == thrd.pad.source:
-                            exists = True
-                    if exists == False:
-                        padThread = Thread(pad)
-                        print("Available device: " + pad.name)
-                        threads.append(padThread)
-                        padThread.start()
-                else: 
-                    padThread = Thread(pad)
-                    print("Added device: " + pad.name)
-                    threads.append(padThread)
-                    padThread.start()
-        else:
-            if printCount == 0:
-                print("Please connect a controller to continue...")
-                printCount = 1
+        while(len(padServ.main) == 0):
+            if padsCopy != padServ.pads:
+                os.system('clear')
+                print('Available Controllers:')
+                padsCopy = padServ.pads.copy()
+                print(padsCopy)
+        newPad = padServ.main[0]
+        inputCopy = newPad.input.copy()
+        while(len(padServ.main) > 0):
+            if inputCopy != newPad.input:
+                os.system('clear')
+                print(newPad.name)
+                inputCopy = newPad.input.copy()
+                print(newPad.input)
+        main()
+
+
+        
 main()
 
 
